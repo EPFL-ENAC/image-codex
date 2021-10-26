81b9c6bad780d65a62d8a0ead2145d3816c0fbc8
David Tang <david.tang@epfl.ch>
2021-10-26 10:49:21 +0200

    about: license

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,7 +1,32 @@
 <template>
   <v-container>
-    <div>
-      <h1>EAST-CO - IT4R</h1>
-    </div>
+    <h1>Image Codex</h1>
+    <p>
+      Image Codex is a project from
+      <a href="https://www.epfl.ch/labs/east/" target="_blank"
+        >Laboratory of Elementary Architecture and Studies of Types (EAST)</a
+      >
+      developed by
+      <a href="https://enacit4r.epfl.ch/" target="_blank">ENAC-IT 4 Research</a
+      >.
+    </p>
+    <h2>License</h2>
+    <p>
+      All uploaded images should be under a
+      <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+        >Creative Commons Attribution 4.0 International License</a
+      >:
+      <a
+        rel="license"
+        href="https://creativecommons.org/licenses/by/4.0/"
+        target="_blank"
+      >
+        <img
+          alt="Creative Commons License"
+          style="border-width: 0"
+          src="https://i.creativecommons.org/l/by/4.0/88x31.png"
+        />
+      </a>
+    </p>
   </v-container>
 </template>
